8d7269c9acf8e35ef793cc29b8beb74a8c0a2360
GammelSami <eefei9ei@account.gammelsami.de>
2022-01-22 23:13:00 +0100

    added h265 handbrakeCLI script that asks for bitrate

---
+++ FFMPEG/5 - h265.bat
@@ -0,0 +1,29 @@
+SET /A trash_uncompressed_files=1
+
+SET /p bitrate="Enter Kbit/s: "
+
+REM @echo off
+if [%1]==[] goto :eof
+:loop
+
+if exist "%~1\*" ( :: if directory given
+  pushd %1
+  for /R %%A in ("*.mp4") do CALL :compress "%%A" "%%~dnpA - COMPRESSED.mp4" :: recursive get files by extension
+) else CALL :compress "%~1" "%~dpn1 - COMPRESSED.mp4"
+
+shift
+if not [%1]==[] goto loop
+exit
+
+
+:: define function
+:compress
+SETLOCAL
+HandBrakeCLI -i "%~1" -o "%~2" -e nvenc_h265 --encoder-preset slow -b %bitrate% --cfr --all-audio -E copy
+:: delete or rename input files
+if /I "%trash_uncompressed_files%" EQU "0" (
+  ren "%~1" "%~n1 - uncompressed.mp4"
+) else (
+  powershell -Command "& {$shell = new-object -comobject 'Shell.Application'; $item = $shell.Namespace(0).ParseName(\"%~1\"); $item.InvokeVerb('delete');}"
+)
+ENDLOCAL :: end of function
