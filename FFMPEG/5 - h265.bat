454014a751c18b9beac6d7dff16691b3f5e25e28
GammelSami <eefei9ei@account.gammelsami.de>
2022-06-07 00:00:57 +0200

    only delete input file if output file exist

--- FFMPEG/5 - h265.bat
+++ FFMPEG/5 - h265.bat
@@ -8,7 +8,7 @@ if [%1]==[] goto :eof
 
 if exist "%~1\*" ( :: if directory given
   pushd %1
-  for /R %%A in ("*.mp4") do CALL :compress "%%A" "%%~dnpA - COMPRESSED.mp4" :: recursive get files by extension
+  for /R %%A in ("*.mp4","*.mkv","*.m4v") do CALL :compress "%%A" "%%~dnpA - COMPRESSED.mp4" :: recursive get files by extension
 ) else CALL :compress "%~1" "%~dpn1 - COMPRESSED.mp4"
 
 shift
@@ -20,10 +20,12 @@ exit
 :compress
 SETLOCAL
 HandBrakeCLI -i "%~1" -o "%~2" -e nvenc_h265 --encoder-preset slow -b %bitrate% --cfr --all-audio --all-subtitles -E copy
-:: delete or rename input files
+:: trash or rename input file
 if /I "%trash_uncompressed_files%" EQU "0" (
-  ren "%~1" "%~n1 - uncompressed.mp4"
+	ren "%~1" "%~n1 - uncompressed.mp4"
 ) else (
-  powershell -Command "& {$shell = new-object -comobject 'Shell.Application'; $item = $shell.Namespace(0).ParseName(\"%~1\"); $item.InvokeVerb('delete');}"
+	if exist "%~2" ( :: if output file exists, delete input file
+		powershell -Command "& {$shell = new-object -comobject 'Shell.Application'; $item = $shell.Namespace(0).ParseName(\"%~1\"); $item.InvokeVerb('delete');}"
+	)
 )
 ENDLOCAL :: end of function
